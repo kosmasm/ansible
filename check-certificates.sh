6393bd6ffcab6476e39e9cf166126cb4e37f89eb
Kosmas Mackrogamvrakis <4059779+kosmasm@users.noreply.github.com>
2022-03-11 13:27:54 +0200

    Update check-certificates.sh

--- check-certificates.sh
+++ check-certificates.sh
@@ -2,7 +2,7 @@
 exitcode=0
 
 for a in $(cat certificate-sites.conf); do
-  days=$(ssl_cert_check.sh expire $a)
+  days=$(./ssl_cert_check.sh expire $a)
   (( $days <= 10 )) && {
   echo WARNING\!\!\! Site $a certificate expiring in $days days. | mailx -s "WARINING!!! Site $a certificate expires in $days days" -S smtp-auth=login -S smtp-use-starttls -S smtp=10.72.3.128 -S from="webmail@kotsovolos.gr" -S smtp-auth-user=webmail@kotsovolos.gr -S smtp-auth-password="555W3bm@il" -S ssl-verify=ignore kosmas@kosmas.gr
   echo WARNING\!\!\! Site $a certificate expiring in $days days.
