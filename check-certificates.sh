572f3f8d98e86b62e2720296903a0f5577607aaa
Kosmas Mackrogamvrakis <4059779+kosmasm@users.noreply.github.com>
2022-03-14 17:48:48 +0200

    Update check-certificates.sh

--- check-certificates.sh
+++ check-certificates.sh
@@ -4,12 +4,12 @@ exitcode=0
 for a in $(cat certificate-sites.conf); do
   days=$(./ssl_cert_check.sh expire $a)
   (( $days <= 10 )) && {
-  echo WARNING\!\!\! Site $a certificate expiring in $days days. | mailx -s "WARINING!!! Site $a certificate expires in $days days" -S smtp-auth=login -S smtp-use-starttls -S smtp=10.72.3.128 -S from="webmail@kotsovolos.gr" -S smtp-auth-user=webmail@kotsovolos.gr -S smtp-auth-password="555W3bm@il" -S ssl-verify=ignore kosmas@kosmas.gr
+  echo WARNING\!\!\! Site $a certificate expiring in $days days. | mailx -s "WARINING!!! Site $a certificate expires in $days days" -S smtp-auth=login -S smtp-use-starttls -S smtp=smtp.ip.address -S from="user@domain.com" -S smtp-auth-user=user@domain.com -S smtp-auth-password="somepassword" -S ssl-verify=ignore kosmas@kosmas.gr
   echo WARNING\!\!\! Site $a certificate expiring in $days days.
   exitcode=2
   }
   (( $days == 15 )) && {
-  echo WARNING\!\!\! Site $a certificate expiring in $days days. | mailx -s "WARINING!!! Site $a certificate expires in $days days" -S smtp-auth=login -S smtp-use-starttls -S smtp=10.72.3.128 -S from="webmail@kotsovolos.gr" -S smtp-auth-user=webmail@kotsovolos.gr -S smtp-auth-password="555W3bm@il" -S ssl-verify=ignore kosmas@kosmas.gr
+  echo WARNING\!\!\! Site $a certificate expiring in $days days. | mailx -s "WARINING!!! Site $a certificate expires in $days days" -S smtp-auth=login -S smtp-use-starttls -S smtp=10.72.3.128 -S from="user@domain.com" -S smtp-auth-user=user@domain.com -S smtp-auth-password="somepassword" -S ssl-verify=ignore kosmas@kosmas.gr
   echo WARNING\!\!\! Site $a certificate expiring in $days days.
   exitcode=1
   }
