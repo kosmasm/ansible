c6d18130e673e3207c85d3e96047eba95baa334a
Kosmas Mackrogamvrakis <4059779+kosmasm@users.noreply.github.com>
2022-10-21 08:58:54 +0300

    Update check-certificates.sh

--- check-certificates.sh
+++ check-certificates.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-for a in $(cat ./check-certificates.conf); do
+for a in $(cat certificate-sites.conf); do
   days=$(ssl_cert_check.sh expire $a)
   (( $days <= 10 )) && {
   echo WARNING\!\!\! Site $a certificate expiring in $days days. | mailx -s "WARINING!!! Site $a certificate expires in $days days" -r support@kosmas.gr -S smtp=127
   }
 done
-for a in $(cat ./check-certificates.conf); do
+for a in $(cat certificate-sites.conf); do
   days=$(ssl_cert_check.sh expire $a)
   (( $days == 15 )) && {
   echo WARNING\!\!\! Site $a certificate expiring in $days days. | mailx -s "WARINING!!! Site $a certificate expires in $days days" -r support@kosmas.gr -S smtp=127
